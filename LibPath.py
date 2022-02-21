4f272836f91179099691cbef987524e65b34020b
Behnam <79264026+beh185@users.noreply.github.com>
2022-02-21 20:54:59 +0330

    for find python's modules path

---
+++ LibPath.py
@@ -0,0 +1,31 @@
+# Github: https: // github.com/beh185
+# Telegram: https: // T.me/dr_xz
+# e-mail: BehnamH.dev@gmail.com
+# ____________________________________________
+
+import os
+import shutil
+name = os.name
+ospath = os.__file__
+ospath = ospath.replace('/os.py', '')
+print('Your modules are in : ', ospath)
+if name == 'posix' : 
+    COPY = input('Do you want to copy RandomColor to there? [We will need sudo access] ')
+    if(COPY == 'Yes' or COPY == 'yes' or COPY == 'YES'):
+        os.system(f'sudo cp -r RandomColor.py {ospath}')
+        print("It's done !")
+    else:
+        print('Copying failed.')
+
+
+
+else:
+    COPY= input('Do you want to copy RandomColor to there? [please open Terminal as adminstore for this operation] ')
+    if( COPY == 'Yes' or COPY == 'yes' or COPY == 'YES' ):
+        shutil.copy('RandomColor.py', ospath)
+        print("It's done !")
+    else:
+        print('Copying failed.')
+
+
+
