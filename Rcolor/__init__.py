fb41c2b5480c9ee2be335922d06b05f7ee3da782
Behnam <79264026+beh185@users.noreply.github.com>
2023-09-14 12:27:26 +0330

    updated

--- Rcolor/__init__.py
+++ Rcolor/__init__.py
@@ -1,1 +1,2 @@
 # !/usr/bin/python
+from .rcolor import color, background, windows_color, windows_background, style, auto_color, auto_background, color_background, color_style, background_style, color_background_style
