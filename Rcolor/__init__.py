0c57332d75c1710549b2eda5722bb3fa514f61a0
Behnam <79264026+beh185@users.noreply.github.com>
2023-09-15 13:31:38 +0330

    Main File

--- Rcolor/__init__.py
+++ Rcolor/__init__.py
@@ -1,2 +1,2 @@
 # !/usr/bin/python
-from .rcolor import color, background, windows_color, windows_background, style, auto_color, auto_background, color_background, color_style, background_style, color_background_style
+from .rcolor import foreground, background, standard_fg, standard_bg, style, auto_fg, auto_bg, foreground_background, foreground_style, foreground_background_style, background_style, rainbow_fg, standard_rainbow_fg
