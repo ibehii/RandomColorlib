5797d384f0e865dfa88f1d1a23b2ea586effbb05
ibehii <behii@tutanota.com>
2024-09-09 01:11:03 +0330

    add __all__

--- Rcolor/__init__.py
+++ Rcolor/__init__.py
@@ -1,2 +1,4 @@
 # !/usr/bin/python
 from .rcolor import foreground, background, standard_fg, standard_bg, style, auto_fg, auto_bg, foreground_background, foreground_style, foreground_background_style, background_style, rainbow_fg, standard_rainbow_fg
+
+__all__ = ['foreground', 'standard_fg', 'background', 'standard_bg', 'style', 'auto_fg', 'auto_bg', 'foreground_background', 'foreground_style', 'foreground_background_style', 'background_style', 'rainbow_fg', 'standard_rainbow_fg']
